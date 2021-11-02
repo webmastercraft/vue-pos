3bebe40b8495d1a60b84cf6837eb0ded15702e95
webmastercraft <iakov.agababian2020@gmail.com>
2021-11-03 01:09:54 +0300

    update cart page with product-list page

--- src/pages/ecommerce/cart.vue
+++ src/pages/ecommerce/cart.vue
@@ -1,90 +1,106 @@
 <template>
   <div>
-    <Breadcrumbs main="Ecommerce" title="Cart"/>
+    <Breadcrumbs main="" title="Cart"/>
     <!-- Container-fluid starts-->
     <div class="container-fluid">
-            <div class="row">
-              <div class="col-sm-12">
-                <div class="card">
-                  <div class="card-header">
-                    <h5>Cart</h5>
-                  </div>
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col-sm-12 empty-cart-cls text-center"  v-if="!cart.length">
-                  <img :src='getImgUrl("ecommerce/icon-empty-cart.png")' class="img-fluid mb-4">
-                  <h3><strong>Your Cart is Empty</strong></h3>
-                  <h4>Add something to make me happy :)</h4>
-                  <router-link :to="'/ecommerce/product'" class="btn btn-primary cart-btn-transform m-t-15">continue shopping</router-link>
-                </div>
-                      <div class="order-history table-responsive wishlist" v-if="cart.length">
-                        <table class="table table-bordered">
-                          <thead>
-                            <tr>
-                              <th>Prdouct</th>
-                              <th>Prdouct Name</th>
-                              <th>Price</th>
-                              <th>Quantity</th>
-                              <th>Action</th>
-                              <th>Total</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr v-for="(item,index) in cart" :key="index">
-                              <td><img class="img-fluid img-40" :src='getImgUrl(item.images[0])' alt="#"></td>
-                              <td>
-                                <div class="product-name"><router-link :to="'/ecommerce/details/'+item.sku">{{item.name}}</router-link></div>
-                              </td>
-                              <td>{{item.price | currency}}</td>
-                              <td>
-                                <fieldset class="qty-box m-auto p-0">
-                                  <div class="input-group addToCart">
-                                    <i class="fa fa-minus btnGtr1"  v-on:click="decrement(item)"></i>
-                                    <input class="touchspin text-center" v-model="item.quantity" name="item.quantity" type="text" >
-                                    <i class="fa fa-plus btnLess1"  v-on:click="increment(item)"></i>
-                                  </div>
-                                </fieldset>
-                              </td>
-                              <td><a href="javascript:void(0)" @click='removeProduct(item)'><feather type="x-circle"></feather></a></td>
-                              <td>{{item.price * item.quantity | currency}}</td>
-                            </tr>
-                            <tr>
-                              <td colspan="4">                                           
-                                <div class="input-group">
-                                  <input class="form-control mr-2" type="text" placeholder="Enter coupan code"><a class="btn btn-primary" href="#">Apply</a>
+      <div class="row">
+        <div class="col-md-12">
+          <landing />
+        </div>
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-12 empty-cart-cls text-center"  v-if="!cart.length">
+                      <img :src='getImgUrl("ecommerce/icon-empty-cart.png")' class="img-fluid mb-4">
+                      <h3><strong>Your Cart is Empty</strong></h3>
+                      <h4>Add something to make me happy :)</h4>
+                      <router-link :to="'/ecommerce/product'" class="btn btn-primary cart-btn-transform m-t-15">continue shopping</router-link>
+                    </div>
+                    <div class="order-history table-responsive wishlist" v-if="cart.length">
+                      <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th>Prdouct</th>
+                            <th>Prdouct Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Action</th>
+                            <th>Total</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="(item,index) in cart" :key="index">
+                            <td><img class="img-fluid img-40" :src='getImgUrl(item.images[0])' alt="#"></td>
+                            <td>
+                              <div class="product-name"><router-link :to="'/ecommerce/details/'+item.sku">{{item.name}}</router-link></div>
+                            </td>
+                            <td>{{item.price | currency}}</td>
+                            <td>
+                              <fieldset class="qty-box m-auto p-0">
+                                <div class="input-group addToCart">
+                                  <i class="fa fa-minus btnGtr1"  v-on:click="decrement(item)"></i>
+                                  <input class="touchspin text-center" v-model="item.quantity" name="item.quantity" type="text" >
+                                  <i class="fa fa-plus btnLess1"  v-on:click="increment(item)"></i>
                                 </div>
-                              </td>
-                              <td class="total-amount">
-                                <h6 class="m-0 text-right"><span class="f-w-600">Total Price :</span></h6>
-                              </td>
-                              <td><span>{{getAmount | currency}}  </span></td>
-                            </tr>
-                            <tr>
-                              <td class="text-right" colspan="5">
-                                <router-link :to="'/ecommerce/product'" class="btn btn-secondary cart-btn-transform">continue shopping</router-link>
-                              </td>
-                              <td>
-                                <router-link :to="'/ecommerce/checkout'" class="btn btn-primary cart-btn-transform"> check out</router-link>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
+                              </fieldset>
+                            </td>
+                            <td class="card-body btn-group-wrapper">
+                              <b-button-group>
+                                <b-dropdown left text="More" variant="light">
+                                  <b-dropdown-item variant="primary">Discount</b-dropdown-item>
+                                  <b-dropdown-item variant="secondary">Edit</b-dropdown-item>
+                                  <b-dropdown-item variant="danger"> Remove</b-dropdown-item>
+                                </b-dropdown>
+                              </b-button-group>
+                            </td>
+                            <td>{{item.price * item.quantity | currency}}</td>
+                          </tr>
+                          <tr>
+                            <!-- <td colspan="4">                                           
+                              <div class="input-group">
+                                <input class="form-control mr-2" type="text" placeholder="Enter coupan code"><a class="btn btn-primary" href="#">Apply</a>
+                              </div>
+                            </td> -->
+                            <td class="total-amount">
+                              <h6 class="m-0 text-right"><span class="f-w-600">Total Price :</span></h6>
+                            </td>
+                            <td><span>{{getAmount | currency}}  </span></td>
+                          </tr>
+                          <!-- <tr>
+                            <td class="text-right" colspan="5">
+                              <router-link :to="'/ecommerce/product'" class="btn btn-secondary cart-btn-transform">continue shopping</router-link>
+                            </td>
+                            <td>
+                              <router-link :to="'/ecommerce/checkout'" class="btn btn-primary cart-btn-transform"> check out</router-link>
+                            </td>
+                          </tr> -->
+                        </tbody>
+                      </table>
                     </div>
                   </div>
-                  <!-- Container-fluid Ends-->
                 </div>
+                <!-- Container-fluid Ends-->
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
     <!-- Container-fluid Ends-->
   </div>
 </template>
 <script>
   import { mapState } from 'vuex';
+  import landing from '../product-list/product-list'
   
   export default {
     name:'Product',
+    components: {
+      landing
+    },
     data(){
       return{
         counter: 1
---
+++ src/pages/product-list/product-list.vue
@@ -0,0 +1,294 @@
+<template>
+  <div @priceVal="pricefilterArray">
+    <div
+      :class="filtered ? 'container-fluid product-wrapper sidebaron' : 'container-fluid product-wrapper'" 
+    >
+      <div class="product-grid">
+        <div v-if="filteredProducts.length == 0" class="slider_views">
+          <div class="shop_slider_view">
+            <div class="silder_title">
+            </div>
+            <Carousel 
+              :autoplay="true" 
+              :per-page="5" 
+              :loop="true" 
+              paginationPosition="bottom-overlay"
+              :perPageCustom="[[280, 1], [360, 2], [992, 5],]"
+            >
+              <Slide
+                v-for="(product,index) in filterProduct"
+                :key="index"
+              >
+                <div class="card">
+                  <div class="product-box">
+                    <div class="product-img">
+                      <!-- <div class="ribbon ribbon-danger" v-if="product.sale">Sale</div> -->
+                      <img class="img-fluid" :src="getImgUrl(product.images[0])" alt />
+                      <div class="product-hover">
+                        <ul>
+                          <router-link :to="'/ecommerce/cart'">
+                            <li @click="addToCart(product)">
+                              <button class="btn" type="button">
+                                <i class="icon-shopping-cart"></i>
+                              </button>
+                            </li>
+                          </router-link>
+                          <li>
+                            <button
+                              class="btn"
+                              @click="quickView(product)"
+                              type="button"
+                              data-toggle="modal"
+                              data-target="#exampleModalCenter"
+                            >
+                              <i class="icon-eye"></i>
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="product-details">
+                      <!-- <div class="rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div> -->
+                      <router-link :to="'/ecommerce/details/'+product.sku">
+                        <h5>{{product.name}}</h5>
+                      </router-link>
+                      <!-- <p>{{product.shortDescription}}</p>
+                      <div class="product-price">
+                        {{product.price | currency}}
+                        <del>{{product.salePrice | currency}}</del>
+                      </div> -->
+                    </div>
+                  </div>
+                </div>
+              </Slide>
+            </Carousel>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- QuickView Modal -->
+    <b-modal size="lg" centered v-model="modalShow" hide-footer hide-header>
+      <button class="close" type="button" v-on:click="quickViewClose(modalShow)">
+        <span>×</span>
+      </button>
+      <div class="product-box quickview row">
+        <div class="product-img col-lg-6">
+           <img class="img-fluid" :src="getImgUrl(modalShow?quickViewProduct.images[0]:'ecommerce/11.jpg')" alt="">
+        </div>
+        <div class="product-details col-lg-6 text-left">
+          <h4>{{quickViewProduct.name}}</h4>
+          <div class="product-price">
+            {{quickViewProduct.price | currency }}
+            <del>{{quickViewProduct.salePrice | currency }}</del>
+          </div>
+          <div class="product-view">
+            <h6 class="f-w-600">Product Details</h6>
+            <p class="mb-0">
+              Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque
+              laudantium, totam rem aperiam eaque ipsa, quae ab illo.
+            </p>
+          </div>
+          <div class="product-size">
+            <ul>
+              <li>
+                <button class="btn btn-outline-light" type="button">M</button>
+              </li>
+              <li>
+                <button class="btn btn-outline-light" type="button">L</button>
+              </li>
+              <li>
+                <button class="btn btn-outline-light" type="button">Xl</button>
+              </li>
+            </ul>
+          </div>
+          <div class="product-qnty">
+            <h6 class="f-w-600">Quantity</h6>
+            <div class="qty-box1">
+              <div class="input-group">
+                <i class="fa fa-minus btnGtr1" @click="decrement()"></i>
+                <input class="touchspin1 text-center" v-model="counter" name="quantity" type="text" />
+                <i class="fa fa-plus btnLess1" @click="increment()"></i>
+              </div>
+            </div>
+            <div class="addcart-btn mt-3">
+              <router-link :to="'/ecommerce/cart'">
+                <button
+                  class="btn btn-primary m-r-10"
+                  type="button"
+                  data-original-title="btn btn-info-gradien"
+                  title
+                  @click="addToCart(quickViewProduct,counter)"
+                >Add To Cart</button>
+              </router-link>
+              <router-link
+                :to="'/ecommerce/checkout'"
+                class="btn btn-primary cart-btn-transform"
+              >Buy Now</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </b-modal>
+  </div>
+</template>
+  <script>
+  import { mapGetters } from 'vuex';
+  import Slider from '../ecommerce/filterbar';
+  import { Carousel, Slide } from 'vue-carousel';
+  
+  export default {
+    name: 'landing',
+    components: {
+      Slider,
+      Carousel,
+      Slide
+    },
+    data() {
+      return {
+        modalShow: false,
+        quickViewProduct: [],
+        counter: 1,
+        priceArray: [],
+        allfilters: [],
+        items: [],
+        filtered: false,
+        col2: false,
+        col3: false,
+        col4: false,
+        col6: true,
+        listViewEnable: false,
+        list: false,
+        value: [0, 450], // filterbar variable
+        searchKey: '',
+        filteredProducts: []
+      };
+    },
+    mounted() {
+      this.pricefilterArray(this.value);
+    },
+    computed: {
+      ...mapGetters({
+        filterProduct: 'products/filterProducts',
+        tags: 'products/setTags'
+      }),
+    },
+    methods: {
+      //search products
+      searchProducts: function()
+      {
+        if(this.searchKey == '')
+        {
+          this.filteredProducts = [];
+          this.listView();
+          this.list = true;
+          return;
+        }
+
+        var searchedProducts = [];
+        
+        for(var i = 0; i < this.filterProduct.length; i++)
+        {
+          var productName = this.filterProduct[i].name.toLowerCase();
+          if(productName.indexOf(this.searchKey.toLowerCase()) >= 0)
+          {
+            searchedProducts.push(this.filterProduct[i]);
+          }
+        }
+
+        this.listView();
+        this.filteredProducts = searchedProducts;
+      },
+
+      //For getting image path
+      getImgUrl(path) {
+        return require('../../assets/images/' + path);
+      },
+
+      // For Order By  
+      onChangeSort(event) {
+        this.$store.dispatch('products/sortProducts', event.target.value);
+      },
+
+      //Filter by Category, Brand, Color
+      allfilter(selectedVal) {
+        this.allfilters = selectedVal;
+        this.$store.dispatch('products/setTags', selectedVal);
+      },
+
+      collapseFilter() {
+        this.filtered = !this.filtered;
+      },
+
+      //Price Filter
+      pricefilterArray(item) {
+        this.$store.dispatch('products/priceFilter', item);
+      },
+    
+      //Add to cart
+      addToCart: function(product, qty) {
+        product.quantity = qty ? qty : 1;
+        this.$store.dispatch('products/addToCart', product);
+      },
+
+      //Quick View
+      quickView: function(product) {
+        this.modalShow = true;
+        return (this.quickViewProduct = product);
+      },
+      quickViewClose: function() {
+        this.modalShow = false;
+      },
+
+      //Quantity increment Decrement
+      increment() {
+        if (this.counter < this.quickViewProduct.stock) this.counter++;
+      },
+
+      decrement() {
+        if (this.counter > 1) this.counter--;
+      },
+
+      //Grid changes
+      grid2() {
+        this.col2 = true;
+        this.col3 = false;
+        this.col4 = false;
+        this.col6 = false;
+        this.listViewEnable = false;
+      },
+      grid3() {
+        this.col2 = false;
+        this.col3 = true;
+        this.col4 = false;
+        this.col6 = false;
+        this.listViewEnable = false;
+      },
+      grid4() {
+        this.col2 = false;
+        this.col3 = false;
+        this.col4 = true;
+        this.col6 = false;
+        this.listViewEnable = false;
+      },
+      grid6() {
+        this.col2 = false;
+        this.col3 = false;
+        this.col4 = false;
+        this.col6 = true;
+        this.listViewEnable = false;
+      },
+      listView() {
+        this.listViewEnable = true;
+        this.list = true;
+        this.col2 = false;
+        this.col3 = false;
+        this.col4 = false;
+        this.col6 = false;
+      },
+      gridView() {
+        this.listViewEnable = false;
+        this.col4 = true;
+      }
+    }
+  };
+</script>
