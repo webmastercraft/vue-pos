66bb0093cf1bcf0c44515584659bb334fb28a6a4
webmastercraft <iakov.agababian2020@gmail.com>
2021-11-03 01:07:47 +0300

    update header layout as design

--- src/components/header/logo.vue
+++ src/components/header/logo.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="header-logo-wrapper">
-    <div class="logo-wrapper">
-      <router-link to="/">
-        <!-- <img class="img-fluid" src="../../assets/images/logo/logo.png" alt /> -->
-        DalililApp
-      </router-link>
+  <div class="profile-nav onhover-dropdown p-0 mr-0">
+    <div class="media profile-media">
+      <div class="media-body">
+        <p class="mb-0 font-roboto">
+          Notifications <i class="middle fa fa-angle-down"></i>
+        </p>
+      </div>
     </div>
-    <div class="toggle-sidebar" @click="toggle_sidebar">
-      <feather
-        class="status_toggle middle sidebar-toggle"
-        type="align-center"
-        id="sidebar-toggle"
-      ></feather>
-    </div>
+    <ul class="profile-dropdown onhover-show-div">
+      <li>
+        <a href="/shops/creat"><span>Admmin Notifications </span></a>
+      </li>
+      <li>
+        <a href="/users/profile"><span>Messages </span></a>
+      </li>
+      <li>
+        <a href="/shops/profile"><span>Comments</span></a>
+      </li>
+    </ul>
   </div>
 </template>
 
--- src/components/header/profile.vue
+++ src/components/header/profile.vue
@@ -1,47 +1,18 @@
 <template>
   <li class="profile-nav onhover-dropdown p-0 mr-0">
     <div class="media profile-media">
-      <img
-        class="b-r-10"
-        src="../../assets/images/dashboard/profile.jpg"
-        alt=""
-      />
       <div class="media-body">
-        <span>Emay Walter</span>
         <p class="mb-0 font-roboto">
-          Admin <i class="middle fa fa-angle-down"></i>
+          Welcome Ali <i class="middle fa fa-angle-down"></i>
         </p>
       </div>
     </div>
     <ul class="profile-dropdown onhover-show-div">
       <li>
-        <a href="/shops/creat"><feather type="archive"></feather><span>New EShop </span></a>
+        <a href="/shops/creat"><feather type="archive"></feather><span>Shop Profile </span></a>
       </li>
       <li>
-        <a href="/users/profile"><feather type="user"></feather><span>Account </span></a>
-      </li>
-      <li>
-        <a href="/shops/profile"><feather type="shopping-cart"></feather><span>My Shop</span></a>
-      </li>
-      <li>
-        <a href="#"
-          ><feather type="file-text"></feather><span>Cashier</span></a
-        >
-      </li>
-      <li>
-        <a href="#">
-          <div class="dropdown shop_list">
-            <feather type="list"></feather><span>Shops list</span>
-            <div class="dropdown-content">
-              <a href="/shops/profile">Link 1</a>
-              <a href="/shops/profile">Link 2</a>
-              <a href="/shops/profile">Link 3</a>
-            </div>
-          </div>
-        </a>
-      </li>
-      <li @click="openCustomizerSetting('settings')">
-        <a href="#"><feather type="settings"></feather><span>Settings</span></a>
+        <a href="/users/profile"><feather type="user"></feather><span>User Profile </span></a>
       </li>
       <li>
         <a @click="logout"
